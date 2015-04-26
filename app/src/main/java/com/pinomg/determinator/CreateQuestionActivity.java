2a7f9816ce2a6e5a5d3601747dc8f4c957e35f19
Ebba Mannheimer <ebbamannheimer@Ebba-Mannheimers-MacBook-Air.local>
2015-04-26 13:58:06 +0200

    New changes regarding CreateQuestion

--- app/src/main/java/com/pinomg/determinator/CreateQuestionActivity.java
+++ app/src/main/java/com/pinomg/determinator/CreateQuestionActivity.java
@@ -4,16 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class CreateQuestionActivity extends Activity {
+
+    private EditText questionField, answer1, answer2;
+    private Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question);
+
+        // Connecting layout with logic
+        questionField = (EditText) findViewById(R.id.questionField);
+        answer1 = (EditText) findViewById(R.id.answer1);
+        answer2 = (EditText) findViewById(R.id.answer2);
+        createButton = (Button) findViewById(R.id.createButton);
     }
 
+    private void sendQuestion(View v) {
+        Question question = new Question();
+        question.question = questionField.getText().toString();
+        question.answerOne = answer1.getText().toString();
+        question.answerTwo = answer2.getText().toString();
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +56,5 @@ public class CreateQuestionActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/pinomg/determinator/Question.java
+++ app/src/main/java/com/pinomg/determinator/Question.java
@@ -7,4 +7,12 @@ public class Question {
     public String question;
     public String answerOne;
     public String answerTwo;
+
+    public Question (String question, String answerOne, String answerTwo) {
+        this.question = question;
+        this.answerOne = answerOne;
+        this.answerTwo = answerTwo;
+    }
+
 }
+
