893140a1ff84253acda4ffd60fd2701eb2cc35d5
Olle Lindeman <olle.lindeman@gmail.com>
2015-04-26 16:51:59 +0200

    Added database contract and basic dbHelper

---
+++ app/src/main/java/com/pinomg/determinator/DbHelper.java
@@ -0,0 +1,26 @@
+package com.pinomg.determinator;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Olle on 26/04/15.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+
+
+    public DbHelper(Context context) {
+        super(context, DbContract.DATABASE_NAME, null, DbContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DbContract.PollEntry.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DbContract.PollEntry.DELETE_TABLE);
+    }
+}
