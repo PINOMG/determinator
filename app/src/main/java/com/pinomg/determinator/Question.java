b980ddb7b2626c30b79a7fe5d352ec1a971b9306
Patrik <patrik.m.olsson@gmail.com>
2015-04-26 11:56:14 +0200

    Created new class for Question.

---
+++ app/src/main/java/com/pinomg/determinator/Question.java
@@ -0,0 +1,10 @@
+package com.pinomg.determinator;
+
+/**
+ * The question class. Holds all information about a question.
+ */
+public class Question {
+    public String question;
+    public String answerOne;
+    public String answerTwo;
+}
