8337d5edccf662005b3fb1c20ddd1679c450911e
Patrik <patrik.m.olsson@gmail.com>
2015-04-26 12:24:33 +0200

    Added toString method to Question class

--- app/src/main/java/com/pinomg/determinator/Question.java
+++ app/src/main/java/com/pinomg/determinator/Question.java
@@ -7,4 +7,9 @@ public class Question {
     public String question;
     public String answerOne;
     public String answerTwo;
+
+    //This is required by the adapter for output in a list.
+    public String toString(){
+        return this.question;
+    }
 }
