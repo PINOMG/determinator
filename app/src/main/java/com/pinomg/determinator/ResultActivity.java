dd49566c5742af75a84f04f591fc0bed22185b5f
Ebba Mannheimer <ebbamannheimer@Ebba-Mannheimers-MacBook-Air.local>
2015-05-11 16:44:02 +0200

    Added activity to show the result of the poll

---
+++ app/src/main/java/com/pinomg/determinator/ResultActivity.java
@@ -0,0 +1,57 @@
+package com.pinomg.determinator;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class ResultActivity extends ActionBarActivity {
+
+    private TextView questionText, answerText;
+    private Poll poll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+       //Connecting layout with logic
+        questionText = (TextView) findViewById(R.id.question);
+        answerText = (TextView) findViewById(R.id.answer);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            poll = (Poll) extras.getSerializable("POLL");
+            questionText.setText(poll.question);
+            answerText.setText(poll.alternativeOne);
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
