e8e6ff458e0ddb41b89115d35db096b4e21de9da
bagaton <bjorn.agaton@gmail.com>
2015-05-17 17:45:00 +0200

    Friends are still checked if back button is pressed and poll created again

--- app/src/main/java/com/pinomg/determinator/AddAnswerersActivity.java
+++ app/src/main/java/com/pinomg/determinator/AddAnswerersActivity.java
@@ -20,6 +20,7 @@ import com.pinomg.determinator.api.ApiHandler;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Objects;
 
 public class AddAnswerersActivity extends Activity {
 
@@ -29,23 +30,14 @@ public class AddAnswerersActivity extends Activity {
     private SparseBooleanArray checked;
     public TextView receiversText;
     private Poll poll;
+    private ListView friendView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_answerers);
 
-        Bundle extras = getIntent().getExtras();
-        if(extras != null) {
-            poll = (Poll) extras.getSerializable("POLL");
-            if( poll.friends != null) {
-                checkedFriends = poll.friends;
-            }
-        } else {
-            Toast.makeText(getBaseContext(), "Error in loading question!", Toast.LENGTH_LONG).show();
-        }
-
-        final ListView friendView = (ListView) findViewById(R.id.friendView);
+        friendView = (ListView) findViewById(R.id.friendView);
         friendView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         createExampleFriendList();
@@ -57,50 +49,39 @@ public class AddAnswerersActivity extends Activity {
 
         checked = friendView.getCheckedItemPositions();
 
-        //Populate checked (SparseBooleanArray) from checkedFriends here...
-        Boolean a = checked.get(1);
-        a = true;
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            poll = (Poll) extras.getSerializable("POLL");
+            if(poll.friends != null) {
+                checkedFriends = poll.friends;
+            }
+        } else{
+            Toast.makeText(getBaseContext(), "Error in loading question!", Toast.LENGTH_LONG).show();
+        }
 
-        //Populate string from checkedFriends here...
+        checkFriends();
+        showCheckedFriends();
 
         friendView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (checked.get(i)) { //Add to checkedFriends
+                if(checked.get(i)) { //Add to checkedFriends
                     checkedFriends.add(friendList.get(i));
 
-                } else { // Remove from checkedFriends
+                } else{ // Remove from checkedFriends
                     checkedFriends.remove(friendList.get(i));
                 }
-
-                String receivers = "";
-
-                for(Iterator<Friend> j = checkedFriends.iterator(); j.hasNext(); ) {
-                    Friend f = j.next();
-                    receivers += f.toString() + " ";
-                }
-
-                receiversText.setText(receivers);
+                showCheckedFriends();
             }
         });
     }
 
-    public void sendPoll(View view) {
-            if (!checkedFriends.isEmpty()) {
-                poll.addFriendlist(checkedFriends);
-                DataApi api = new DataApi(getBaseContext());
-                api.addPoll(poll);
-                setResult(RESULT_OK, null);
-                finish();
-            } else {
-                Toast.makeText(getBaseContext(), "Haven't you got any friends, or?", Toast.LENGTH_LONG).show();
-            }
-    }
-
+    //Attaches the checked friends if back button is pressed.
     @Override
     public void onBackPressed() {
         Intent intent = new Intent();
-        intent.putExtra("POLLEN", poll);
+        poll.friends = checkedFriends;
+        intent.putExtra("POLLEN", checkedFriends);
         setResult(RESULT_CANCELED, intent);
         finish();
     }
@@ -127,10 +108,51 @@ public class AddAnswerersActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    //Attach friends to the poll and upload it to the database.
+    public void sendPoll(View view) {
+        if (!checkedFriends.isEmpty()) {
+            poll.addFriendlist(checkedFriends);
+            DataApi api = new DataApi(getBaseContext());
+            api.addPoll(poll);
+            setResult(RESULT_OK, null);
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Haven't you got any friends, or?", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Populate string showed in the TextView receivers from checkedFriends here
+    public void showCheckedFriends () {
+        String receivers = "";
+        for(Iterator<Friend> j = checkedFriends.iterator(); j.hasNext(); ) {
+            Friend f = j.next();
+            receivers += f.toString() + " ";
+        }
+        receiversText.setText(receivers);
+    }
+
+    //Match the already checked friends attached in the
+    //poll with the corresponding friends in the ListView
+    //and check the boxes for the identified matches.
+    public void checkFriends() {
+        for(Iterator<Friend> i = checkedFriends.iterator(); i.hasNext();) {
+            int index = 0;
+            Friend friend1 = i.next();
+            for(Iterator<Friend> j = friendList.iterator(); j.hasNext();) {
+                Friend friend2 = j.next();
+                if(friend1.equals(friend2)) {
+                    Log.e("Equals", "Equals!");
+                    friendView.setItemChecked(index, true);
+                }
+                index++;
+            }
+        }
+    }
+
     public void createExampleFriendList() {
-        Friend friend1 = new Friend("Rasmus", 123);
-        Friend friend2 = new Friend("Karin", 456);
-        Friend friend3 = new Friend("Bubba", 789);
+        Friend friend1 = new Friend("Rasmus");
+        Friend friend2 = new Friend("Karin");
+        Friend friend3 = new Friend("Bubba");
 
         friendList.add(friend1);
         friendList.add(friend2);
