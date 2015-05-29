3467e89c9a3f9b45346c57fc29f63d8a9ece61fb
Olle Lindeman <olle.lindeman@gmail.com>
2015-05-29 11:48:54 +0200

    AsyncTask for AnswerQuestion

--- app/src/main/java/com/pinomg/determinator/activity/LoginActivity.java
+++ app/src/main/java/com/pinomg/determinator/activity/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
